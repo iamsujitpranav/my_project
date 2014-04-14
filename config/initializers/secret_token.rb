02601ef6e28f5c8e522f93d20d2b9a36cb848a68
sujitpranav <sujitpranav@kreatio.com>
2014-04-14 16:52:13 +0530

    updating my project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyProject::Application.config.secret_key_base = '2bb786bbe11b634fdce7c01d51306d19efacfdbeff9167ddf9ef7f67f97da3262d75180607a7b808709207e38a8563fc3123bdeacd502b652c8be85ef6010353'
